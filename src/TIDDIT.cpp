eafaf8388eae39700511b6a597782eda61c57dc0
J35P312 <jesper.eisfeldt@scilifelab.se>
2017-12-13 11:57:08 +0100

    modified:   TIDDIT.py 	modified:   src/TIDDIT.cpp 	modified:   src/data_structures/Translocation.cpp

--- src/TIDDIT.cpp
+++ src/TIDDIT.cpp
@@ -45,7 +45,7 @@ int main(int argc, char **argv) {
 	float insertStd;
 	int min_variant_size= 100;
 	string outputFileHeader ="output";
-	string version = "2.0.2";
+	string version = "2.0.3";
 	
 	//collect all options as a vector
 	vector<string> arguments(argv, argv + argc);
